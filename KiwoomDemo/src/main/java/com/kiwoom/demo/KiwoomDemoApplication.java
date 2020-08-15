b7e6ddd176972fd59000bfadb81a5bb772f39b45
ihyeon-u <ihyeon-u@localhost>
2020-08-16 00:50:00 +0900

    2020/08/15 1st commit

---
+++ KiwoomDemo/src/main/java/com/kiwoom/demo/KiwoomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kiwoom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwoomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwoomDemoApplication.class, args);
+	}
+
+}
