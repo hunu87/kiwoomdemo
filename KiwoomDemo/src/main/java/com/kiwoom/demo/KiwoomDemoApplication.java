1c61c204c2f080c55657a0581bae1fd9c8a729a5
ihyeon-u <ihyeon-u@localhost>
2020-09-21 22:32:35 +0900

    20200921 war 배포 설정

--- KiwoomDemo/src/main/java/com/kiwoom/demo/KiwoomDemoApplication.java
+++ KiwoomDemo/src/main/java/com/kiwoom/demo/KiwoomDemoApplication.java
@@ -2,12 +2,18 @@ package com.kiwoom.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KiwoomDemoApplication {
+public class KiwoomDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KiwoomDemoApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(KiwoomDemoApplication.class);
+	}
 }
