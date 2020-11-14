8af08c3bf21103e1c4f6b5970c88eb4f9cb6ed65
ihyeon-u <ihyeon-u@172.30.1.34>
2020-11-14 22:47:51 +0900

    20201114 UrlController 수정

--- KiwoomDemo/src/main/java/com/kiwoom/demo/home/controller/UrlController.java
+++ KiwoomDemo/src/main/java/com/kiwoom/demo/home/controller/UrlController.java
@@ -1,5 +1,6 @@
 package com.kiwoom.demo.home.controller;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -10,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -22,36 +25,58 @@ public class UrlController {
 	@Autowired
 	private RequestMappingHandlerMapping handlerMapping;
 	
+	/**
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping("/getAllRequestUrl")
 	@ResponseBody
 	public List<HashMap<String, Object>> getAllRequestUrl(HttpServletRequest request){
 		List<HashMap<String, Object>> mappingInfoList = new ArrayList<HashMap<String, Object>>();
 		
-		RequestMappingInfo requestMappingInfo = null; 
-		Set<String> patterns; 
-		Object[] sArr; 
-		String url, beanName;
+		RequestMappingInfo requestMappingInfo = null;
+		
+		Set<String> patterns;
+		Set<RequestMethod> methods;
+		
+		Object[] urlArr = null;
+		Object[] methodArr = null;
+		
+		String controllerName, url, method;
 		
 		Map<RequestMappingInfo, HandlerMethod> handlerMap = handlerMapping.getHandlerMethods();
 		Iterator<RequestMappingInfo> it = handlerMap.keySet().iterator();
 		
 		while(it.hasNext()) { 
-			requestMappingInfo = it.next(); 
+			requestMappingInfo = it.next();
+			
 			patterns = requestMappingInfo.getPatternsCondition().getPatterns();
+			methods = requestMappingInfo.getMethodsCondition().getMethods();
 			
 			if(!patterns.isEmpty()) { 
-				sArr = patterns.toArray(); 
-				if(sArr.length == 1) { 
+				urlArr = patterns.toArray();
+				
+				if(!methods.isEmpty()) {
+					methodArr = methods.toArray();
+				}else {
+					methodArr[0] = "";
+				}
+				
+				if(urlArr.length == 1) { 
 					HashMap<String,Object> map = new HashMap<String,Object>(); 
 					
-					// annotaion에 지정된 URL 값
-					url = (String) sArr[0];
+					// Controller Name
+					controllerName = (String) handlerMap.get(requestMappingInfo).getBean();
 					
-					// URL이 지정되어있는 컨트롤러 이름
-					beanName = (String) handlerMap.get(requestMappingInfo).getBean();
+					// URL
+					url = (String) urlArr[0];
 					
-					map.put("controller", beanName);
+					// HTTP Request Method
+					method = methodArr[0].toString();
+					
+					map.put("controller", controllerName);
 					map.put("url", url);
+					map.put("httpMethod", method);
 					
 					mappingInfoList.add(map);
 				} 
