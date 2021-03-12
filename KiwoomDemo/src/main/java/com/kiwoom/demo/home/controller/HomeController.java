515bbb39e6270e2276aa14f65bc8fcac3de19e1b
이현우 <ihyeon-u@ihyeon-uui-MacBookPro.local>
2021-03-12 23:41:04 +0900

    pathvariable test

--- KiwoomDemo/src/main/java/com/kiwoom/demo/home/controller/HomeController.java
+++ KiwoomDemo/src/main/java/com/kiwoom/demo/home/controller/HomeController.java
@@ -2,13 +2,25 @@ package com.kiwoom.demo.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class HomeController {
 	
 	@RequestMapping(value = "/home") 
-	public String home(Model model) throws Exception{ 
+	public String home(@RequestParam String param, Model model) throws Exception{
+		log.info("param : " + param);
 		return "/home/home";
+	}
+	
+	@RequestMapping("/home/{no}")
+	public String pathVariableTest(@PathVariable String no) {
+		return "redirect:/home?param=123";
 	}
 }
